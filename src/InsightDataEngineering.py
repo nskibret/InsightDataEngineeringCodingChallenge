540f72ed87c71b3c50db32d77994ebde03a31d50
nskibret <nadewsikuwaru21@gmail.com>
2019-04-01 15:12:43 -0400

    implement read_data function

--- src/InsightDataEngineering.py
+++ src/InsightDataEngineering.py
@@ -6,20 +6,74 @@ Created on Sat Mar 30 10:44:16 2019
 @author: nskibret
 """
 
+import csv
+
 class PurchaseAnalytics(object):
     
-    def __init__(self, input_dir, output_dir):
+    def __init__(self, input_file_dir, output_file_dir):
         
-        self.input_dir = input_dir
-        self.output_dir = output_dir
+        self.input_dir = input_file_dir
+        self.output_dir = output_file_dir
+        
+        #a dictionary
+        self._products = {}
+        self._order_products = {}
+        self.department_id = []
+        self.stat_per_department = {}
         
     
     def read_data(self):
         
+        
+        with open(self.input_dir) as data:
+            purchase_data  = csv.reader(data, delimiter=',')
+           
+            for row in purchase_data:
+                self._products[row[1]] = row[3]
+            del self._products['product_id']
+            
+            
     
         
         
-    def write_data(self):
+    def write_data_to_disk(self):
+        
+        
+        pass
+        
+    
+    def find_order_statistics(self):
+        
+        pass
+        
+        
+        
+        
+        
+        
+    def calculate_percentage_values(self):
+        
+        
+        pass
+    
+    def print_data(self):
+        print("Product ID          Reordered")
+        for key in self._products:
+            print(" {}:  {}".format(key, self._products[key]))
+        
+        
+        
+        
+        
+
+        
+file = 'order_products.csv'
+
+test = PurchaseAnalytics(file, './')
+test.read_data()
+test.print_data()
+        
+        
         
         
     

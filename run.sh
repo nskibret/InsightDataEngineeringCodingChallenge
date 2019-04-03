fe9bdb624b0719bd562be83b11c53859eb7631be
Nadew S Kibret <nskibret@users.noreply.github.com>
2019-04-03 00:53:37 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 
 
-python3 ./src/run_purchase_analytics.py ./input/ ./output products.csv order_products.csv
+python3 ./src/run_purchase_analytics.py ./input/ ./output/ products.csv order_products.csv

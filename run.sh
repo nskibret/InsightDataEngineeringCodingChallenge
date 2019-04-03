d32d5d0e47b810cb17430f7c112aa915a67c4b5f
Nadew S Kibret <nskibret@users.noreply.github.com>
2019-04-03 00:35:36 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. 
-#
-#python ./src/purchase_analytics.py ./input/order_products.csv ./input/products.csv ./output/report.csv
+# 
+
 python3 ./src/run_purchase_analytics.py ./input/ ./output products.csv order_products.csv
